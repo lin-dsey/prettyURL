fc8646f0e93aea5de01bf46cfeda1e1b53c53824
Lindsey <84740121+lin-dsey@users.noreply.github.com>
2022-02-16 18:40:41 -0600

    Update shortUrl.js

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-const shortID = require('shortid')
+const shortID = require('shortID')
 
 const shortUrlSchema = new mongoose.Schema({
   full: {type: String, required: true},
-  short: {type: String, required: true, default: shortID.generate },
-  clicks: {type: Number, required: true, default: 0 }
+  short: {type: String, required: true, default: shortID.generate},
+  clicks: {type: Number, required: true, default: 0}
 })
 
 module.exports = mongoose.model('ShortUrl', shortSchema)
